f88f23065115e85786d740316f53e393bc08a993
Ashish Soni <ashishsoni243k@gmail.com>
2026-03-24 12:37:23 +0530

    Time: 11 ms (75.84%) | Memory: 19.4 MB (87.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # method 1
         # s = s.lower()
         # t = t.lower()
         # count = [0]*26
@@ -13,14 +14,38 @@ class Solution:
         #     if num != 0:
         #         return False
         # return True 
-        list1 = list(s)
-        list2 = list(t)
-        list1.sort()
-        list2.sort()
-        if list1 == list2:
-            return True
-        else:
+
+        # method 2
+        # list1 = list(s)
+        # list2 = list(t)
+        # list1.sort()
+        # list2.sort()
+        # if list1 == list2:
+        #     return True
+        # else:
+        #     return False
+
+        # method 3
+        freq = {}
+        if len(s) != len(t):
             return False
+        for i in s:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i] +=1
+        for i in t:
+            if i not in freq:
+                return False
+            else:
+                freq[i] -= 1
+        
+        for i in freq.values():
+            if i!=0 :
+                return False
+        return True
+
+
 
 
         
