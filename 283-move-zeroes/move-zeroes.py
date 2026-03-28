df3ea4517db08e9dae65ed894568ba6bf941aa07
Ashish Soni <ashishsoni243k@gmail.com>
2026-03-28 15:10:06 +0530

    Time: 4 ms (60.84%) | Memory: 20.5 MB (77.74%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,11 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        arr1 = []
-        arr2 = []
-        for i in range (len(nums)):
-            if nums[i] == 0:
-                arr1.append(0)
-            else:
-                arr2.append(nums[i])
-        nums[:] = arr2 +arr1
+        # arr1 = []
+        # arr2 = []
+        # for i in range (len(nums)):
+        #     if nums[i] == 0:
+        #         arr1.append(0)
+        #     else:
+        #         arr2.append(nums[i])
+        # nums[:] = arr2 +arr1
+        insert = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[insert],nums[i] = nums[i],nums[insert]
+                insert  += 1
