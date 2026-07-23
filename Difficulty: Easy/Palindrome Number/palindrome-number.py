11f173d5b25385ffa49b5a7aba0d90ac6c4903ad
Ashish Soni <ashishsoni243k@gmail.com>
2026-07-23 21:07:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Number/palindrome-number.py
+++ Difficulty: Easy/Palindrome Number/palindrome-number.py
@@ -2,8 +2,15 @@ class Solution:
     def isPalindrome(self, n):
 		# code here
 		n = abs(n)
-		temp = str(n)
-		
-		return temp == temp[::-1]
-		
-		
+		replica = abs(n)
+		ans = 0
+		if -9<=n<=9:
+		    return True
+		while replica>0:
+		    temp = replica%10
+		    ans = ans*10+temp
+		    replica = replica//10
+		if ans == n:
+		    return True
+		return False
+		    
