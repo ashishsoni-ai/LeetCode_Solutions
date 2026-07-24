86a67693c75e8b07d50b47bb4e2018f63a641b88
Ashish Soni <ashishsoni243k@gmail.com>
2026-07-24 10:54:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthMissing(self, arr, k):
+        n = len(arr)
+        l, r = 0, n - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            missing = arr[mid] - (mid + 1)
+
+            if missing < k:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return l + k
