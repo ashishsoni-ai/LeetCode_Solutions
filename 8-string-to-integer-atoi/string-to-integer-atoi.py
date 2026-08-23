88b9fd5584a4da164c2e1dae8d6da211b4fb1610
Ashish Soni <ashishsoni243k@gmail.com>
2026-08-23 10:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (45.52%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,37 +1,28 @@
 class Solution:
-    def myAtoi(self, s: str, i=0, num=0, sign=1, started=False) -> int:
-        INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
+    def myAtoi(self, s: str) -> int:
+        i = 0
+        n = len(s)
+        while i<n and s[i]==" ":
+            i+=1
+        sign = 1
+        if i<n and s[i] =="-":
+            sign = -1
+            i += 1
+        elif i<n and s[i] =="+":
+            sign = 1 
+            i += 1
+        num = 0
+        while i<n and ord("0")<=ord(s[i])<=ord("9"):
+            digit = ord(s[i])-ord("0")
+            num = num * 10 + digit
+            i += 1
 
-        # Skip spaces and find sign
-        if not started:
-            while i < len(s) and s[i] == ' ':
-                i += 1
+        num = num*sign
 
-            if i < len(s) and s[i] == '-':
-                sign = -1
-                i += 1
-            elif i < len(s) and s[i] == '+':
-                i += 1
 
-            started = True
-
-        # Stop at end or non-digit
-        if i >= len(s) or s[i] < '0' or s[i] > '9':
-            return sign * num
-
-        # Convert character to digit
-        digit = ord(s[i]) - ord('0')
-
-        # Build number
-        num = num * 10 + digit
-
-        # Check overflow
-        if sign == 1 and num > INT_MAX:
-            return INT_MAX
-
-        if sign == -1 and -num < INT_MIN:
-            return INT_MIN
-
-        # Recursive call
-        return self.myAtoi(s, i + 1, num, sign, started)
+        if num<-2**31:
+            return -2**31
+        if num > 2**31-1:
+            return 2**31-1
+        return num
+        
