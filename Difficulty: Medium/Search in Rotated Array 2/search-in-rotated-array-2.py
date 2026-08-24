fb0c92eadbf7796eedcafbd242a2ec2b0263d29b
Ashish Soni <ashishsoni243k@gmail.com>
2026-08-24 10:39:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.py
+++ Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.py
@@ -1,9 +1,29 @@
 class Solution:
     def search(self, arr, key):
         # code here
-        ans = False
         n = len(arr)
-        for i in range(n):
-            if arr[i] == key:
-                ans = True
-        return ans
+        low = 0
+        high = n-1
+        while low <= high:
+            mid = (low + high)//2
+            if arr[mid] == key:
+                return True
+            if arr[low] == arr[mid] and arr[high]==arr[mid]:
+                low = low+1
+                high = high -1
+                continue
+            if arr[low]<=arr[mid]:
+                if arr[low]<= key < arr[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+                    
+            else:
+                if arr[mid] <= key <= arr[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+                    
+        return False
+                    
+                
