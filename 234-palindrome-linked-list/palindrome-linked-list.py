93dfad114b20fca9da3e39b587303182b0b37f89
Ashish Soni <ashishsoni243k@gmail.com>
2026-09-05 11:34:52 +0530

    Time: 36 ms (50.52%) | Memory: 42.1 MB (96.61%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,20 +1,46 @@
 class Solution:
+    def findmid(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+    def reverse(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
+
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        temp = head
 
-        # Put all values into stack
-        while temp is not None:
-            stack.append(temp.val)
-            temp = temp.next
+        if head is None or head.next is None:
+            return True
 
-        # Compare linked list with stack
-        temp = head
-        while temp is not None:
-            if temp.val != stack[-1]:
+        # Find middle
+        mid = self.findmid(head)
+
+        # Reverse second half
+        second_half = self.reverse(mid)
+
+        # Compare both halves
+        first = head
+        second = second_half
+
+        while second:
+            if first.val != second.val:
                 return False
 
-            temp = temp.next
-            stack.pop()
+            first = first.next
+            second = second.next
 
         return True
