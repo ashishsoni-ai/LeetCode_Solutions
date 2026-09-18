9fd9880031ef9073b7cd57607bf8f0148fbd0fa9
Ashish Soni <ashishsoni243k@gmail.com>
2026-09-18 21:13:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Median in a Row-Wise Sorted Matrix/median-in-a-row-wise-sorted-matrix.py
+++ Difficulty: Medium/Median in a Row-Wise Sorted Matrix/median-in-a-row-wise-sorted-matrix.py
@@ -1,13 +1,42 @@
 class Solution:
+
+    def solveSmaller(self, AssumedMid, mat):
+        count = 0
+
+        for i in range(len(mat)):
+            start = 0
+            end = len(mat[i]) - 1
+
+            while start <= end:
+                mid = (start + end) // 2
+
+                if mat[i][mid] <= AssumedMid:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+            count += start
+
+        return count
+
     def median(self, mat):
-    	# code here 
-    	list1 = []
-    	for i in range(len(mat)):
-    	    for j in range(len(mat[0])):
-    	        list1.append(mat[i][j])
-    	list1.sort()        
-    	median = len(list1)//2
-    	return list1[median]
-    	        
-    	
-    	        
+        R = len(mat)
+        C = len(mat[0])
+
+        N = R * C
+        medianIndex = (N // 2) + 1
+
+        start = 1
+        end = 2000
+
+        while start <= end:
+            AssumedMid = (start + end) // 2
+
+            CurrentSmaller = self.solveSmaller(AssumedMid, mat)
+
+            if CurrentSmaller < medianIndex:
+                start = AssumedMid + 1
+            else:
+                end = AssumedMid - 1
+
+        return start
