023a33af5f89d77221d9869caeb038d9b4d2f73e
Ashish Soni <ashishsoni243k@gmail.com>
2026-09-25 08:54:02 +0530

    Time: 5 ms (50.02%) | Memory: 20 MB (24.03%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,20 +1,17 @@
 class Solution:
     def maxProduct(self, nums):
-        curr_max = nums[0]
-        curr_min = nums[0]
-        ans = nums[0]
+        n = len(nums)
+        ans = float("-inf")
+        prefix = 1
+        suffix = 1
+        for i in range(n):
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+            prefix = prefix * nums[i]
+            suffix = suffix * nums[n-i-1]
 
-        for i in range(1, len(nums)):
-            x = nums[i]
-
-            # Save old values before updating
-            temp_max = curr_max
-            temp_min = curr_min
-
-            curr_max = max(x, x * temp_max, x * temp_min)
-            curr_min = min(x, x * temp_max, x * temp_min)
-
-            ans = max(ans, curr_max)
-
+            ans = max(ans,max(prefix,suffix))
         return ans
         
